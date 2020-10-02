8402e97ef2db533cf4fffc980dee3906cce448e1
Jose Salazar Mendez <thatsenpai@Joses-MBP.eau.wi.charter.com>
2020-10-01 19:16:04 -0500

    connect cart

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -3,6 +3,8 @@ const itemsContainer = document.getElementById('items')
 const itemList = document.getElementById('item-list')
 const cartQty = document.getElementById('cart-qty')
 const cartTotal = document.getElementById('cart-total')
+const addForm = document.getElementById('add-form')
+
 itemList.innerHTML = '<li> Hello World</li>'
 
 for (let i=0; i<data.length; ++i) {
@@ -38,6 +40,7 @@ cart.innerText = "Add to Cart"
 newDiv.appendChild(cart)
 
 const all_items_button = Array.from(document.querySelectorAll("button"))
+
 all_items_button.forEach(elt => elt.addEventListener('click', () => {
     addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
     showItems()
@@ -45,19 +48,52 @@ all_items_button.forEach(elt => elt.addEventListener('click', () => {
 
 }
 
-
-
 const cart = [ ]
+
+itemList.onchange = function(e){
+    if(e.target && e.target.classList.contains('update')){
+        const name = e.target.dataset.name 
+        const qty = parseInt(e.target.value)
+        updateCart(name, qty)
+    }
+}
+
+itemList.oneclick = function(e){
+    console.log("cLICK")
+    if(e.target && e.target.classList.contains('remove')){
+        const name = e.target.dataset.name
+    removeItem(name)
+    }
+    else if(e.target && e.target.classList.contains('add-one')){
+        const name = e.target.dataset.name
+        addItem(name)
+
+    }else if(e.target && e.target.classList.contains('remove-one')){
+        const name = e.target.dataset.name
+        removeItem(name, 1)
+
+    }
+
+}
+
+addForm.onsubmit = function(e){
+    e.preventDefault()
+    const name = itemName.value
+    const price = itemPrice.value
+    addItem(name, price)
+}
 
 function addItem(num, price){
     for( let i =0; i <cart.length; i +=1 ){
         if (cart[i].name === name){
             cart[i].qty +=1
+            showItems()
             return
         }
     }
     const item = {name:name, price :price, qty: 1}
     cart.push(name)
+    showItems()
 }
 
 
@@ -74,10 +110,12 @@ function showItems(){
        //console.log(` ${cart[i].name} $${cart[i].price} x ${cart[i].qty} `)
        const{ name , price, qty} = cart[i]
 
-
-
-        itemStr += `<li> ${name} $${price} x ${qty} 
-         = ${qty * price} </li>`
+        itemStr += `<li> ${name} $${price} x ${qty} = ${qty * price}
+         <button clas="remove" data-name="${name}">Remove</button>
+         <button clas="add-one" data-name="${name}">+</button>
+         <button clas="remove-one" data-name="${name}">-</button>
+         <input class="update" type="number">
+         </li>`
     }
     itemList.innerHTML = itemStr
 
@@ -89,19 +127,23 @@ function showItems(){
 
 function getQty(){
     let qty = 0
-    for(let i =0; i < cart.length; i += 1){
+    for(let i =0; i < cart.length; i -= 1){
         qty +=cart[i].qty
     }
     return qty
 
 }
+
+
 function getTotal(){
     let total =0
-    for (let i = 0; i < cart.length; i +=1){
+    for (let i = 0; i < cart.length; i -=1){
         total += cart[i].price * cart[i].qty
     }
     return total.toFixed(2)
 }
+
+
 function removeItem(name, qty = 0){
     for (let i = 0; i < cart.length; i +=1){
         if(cart[i].name === name){
@@ -112,7 +154,24 @@ function removeItem(name, qty = 0){
             if(cart[i].qty < 1){
                 cart.splice(i, 1)
             }
+            showItems()
             return
         }
+    }
+}
+function updateCart(name, qty){
+    for(let i= 0; i < cart.length; i += 1){
+        if(cart[i].name === name){
+            if(qty < 1){
+                removeItem(name)
+                return
+
+            }
+            cart[i].qty = qty
+            showItems()
+            return
+
+        }
+
     }
 }
